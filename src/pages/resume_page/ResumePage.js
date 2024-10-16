ae6e8fce9a3bbbecc400bd79b16bca2b53d3a4fd
tracy <tracyolivia@msn.com>
2024-10-16 09:25:02 +0200

    Nochmal

--- src/pages/resume_page/ResumePage.js
+++ src/pages/resume_page/ResumePage.js
@@ -2,7 +2,7 @@ import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import Particle from "../../Particle";
-import pdf from "../../assets/Lebenslauf Martina Schulz.pdf";
+import pdf from "../../assets/";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import Zoom from "react-reveal/Zoom";
